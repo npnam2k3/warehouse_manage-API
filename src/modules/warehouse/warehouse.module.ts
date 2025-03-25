f5e6f2ef85624df9e5965ae7ee4ab48acd10cafe
phuongnam <nguyenphuongnam284@gmail.com>
2025-03-26 06:58:56 +0700

    feat(category): update feature --- feat(warehouse): crud feature

---
+++ src/modules/warehouse/warehouse.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { WarehouseService } from './warehouse.service';
+import { WarehouseController } from './warehouse.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Warehouse } from './entities/warehouse.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Warehouse])],
+  controllers: [WarehouseController],
+  providers: [WarehouseService],
+})
+export class WarehouseModule {}
---
+++ src/modules/warehouse/warehouse.service.ts
@@ -0,0 +1,107 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateWarehouseDto } from './dto/create-warehouse.dto';
+import { UpdateWarehouseDto } from './dto/update-warehouse.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Warehouse } from './entities/warehouse.entity';
+import { ERROR_MESSAGE } from 'src/constants/exception.message';
+import { ENTITIES_MESSAGE } from 'src/constants/entity.message';
+
+@Injectable()
+export class WarehouseService {
+  constructor(
+    @InjectRepository(Warehouse)
+    private readonly warehouseRepository: Repository<Warehouse>,
+  ) {}
+  async create(createWarehouseDto: CreateWarehouseDto) {
+    const warehouseExists = await this.warehouseRepository.findOne({
+      where: {
+        name: createWarehouseDto.name,
+      },
+    });
+    if (warehouseExists)
+      throw new ConflictException(
+        ERROR_MESSAGE.ALREADY_EXISTS(ENTITIES_MESSAGE.WAREHOUSE),
+      );
+
+    const newWarehouse = this.warehouseRepository.create({
+      name: createWarehouseDto.name,
+    });
+    if (createWarehouseDto.address)
+      newWarehouse.address = createWarehouseDto.address;
+
+    return await this.warehouseRepository.save(newWarehouse);
+  }
+
+  async findAll() {
+    const warehouses = await this.warehouseRepository.find();
+
+    return warehouses;
+  }
+
+  async findOne(id: number) {
+    const warehouseExists = await this.warehouseRepository.findOne({
+      where: { id },
+    });
+    if (!warehouseExists)
+      throw new NotFoundException(
+        ERROR_MESSAGE.NOT_FOUND(ENTITIES_MESSAGE.WAREHOUSE),
+      );
+
+    return warehouseExists;
+  }
+
+  async update(id: number, updateWarehouseDto: UpdateWarehouseDto) {
+    if (!updateWarehouseDto || Object.keys(updateWarehouseDto).length === 0)
+      throw new BadRequestException(ERROR_MESSAGE.INVALID_INPUT_UPDATE);
+
+    const warehouseExists = await this.warehouseRepository.findOne({
+      where: { id },
+    });
+    if (!warehouseExists)
+      throw new NotFoundException(
+        ERROR_MESSAGE.NOT_FOUND(ENTITIES_MESSAGE.WAREHOUSE),
+      );
+
+    const existingWarehouse = await this.warehouseRepository
+      .createQueryBuilder('warehouse')
+      .where('LOWER(warehouse.name) = LOWER(:name)', {
+        name: updateWarehouseDto.name,
+      })
+      .andWhere('warehouse.id != :id', { id })
+      .getOne();
+    if (existingWarehouse)
+      throw new ConflictException(
+        ERROR_MESSAGE.ALREADY_EXISTS(ENTITIES_MESSAGE.WAREHOUSE),
+      );
+
+    let isUpdated = false;
+
+    Object.entries(updateWarehouseDto)?.forEach(([key, value]) => {
+      if (value && warehouseExists[key] !== value) {
+        warehouseExists[key] = value;
+        isUpdated = true;
+      }
+    });
+    if (!isUpdated) throw new BadRequestException(ERROR_MESSAGE.NO_DATA_CHANGE);
+
+    await this.warehouseRepository.update(id, warehouseExists);
+    return warehouseExists;
+  }
+
+  async remove(id: number) {
+    const warehouseExists = await this.warehouseRepository.findOne({
+      where: { id },
+    });
+    if (!warehouseExists)
+      throw new NotFoundException(
+        ERROR_MESSAGE.NOT_FOUND(ENTITIES_MESSAGE.WAREHOUSE),
+      );
+    await this.warehouseRepository.softRemove(warehouseExists);
+  }
+}
